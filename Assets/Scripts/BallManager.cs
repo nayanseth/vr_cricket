f8f152a200a302979617dc2ecf5262f3627b3dfe
Nayan Seth <nayantronix@gmail.com>
2017-03-26 05:04:53 -0700

    Fours and Sixes Calculator

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour {
 
-	TrailRenderer tail;
-
+	TrailRenderer trail;
+	bool batHitFlag;
+	int ballBounceCount, score;
+	float distance;
+	VariableManager vm;
+	TextManager tm;
 	// Use this for initialization
 	void Start () {
-		tail = this.gameObject.GetComponent<TrailRenderer> ();
+		batHitFlag = false;
+		ballBounceCount = 0;
+		vm = GameObject.Find("Managers").GetComponent<VariableManager> ();
+		tm = GameObject.Find("Managers").GetComponent<TextManager> ();
+		trail = this.gameObject.GetComponent<TrailRenderer> ();
 		Destroy (this.gameObject, 5f);
 
+	}
+
+	void FixedUpdate() {
+		distance = Vector3.Magnitude (this.gameObject.transform.position);
+
+		if (distance > 85f && batHitFlag) {
+			batHitFlag = false;
+			score = vm.GetScoreCount ();
+			if (ballBounceCount > 0) {
+				score += 3;
+			} else {
+				score += 5;
+			}
+
+			vm.SetScoreCount (score);
+			tm.SetScoreText (score.ToString());
+		}
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Bat") {
+			batHitFlag = true;
+		}
+
+		if (batHitFlag && other.gameObject.name == "Stadium Terrain") {
+			ballBounceCount++;
+		}
 	}
 
 }
